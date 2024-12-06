8f7dcd05ed5b7d05c83858a253fbce2791ddd7e1
Trisno <trisno@stockbit.com>
2024-12-07 00:19:55 +0700

    fix: linter and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => (
-  <div className='p-4'>
-    <h1 className='text-3xl font-bold text-blue-600'>Hello Tailwind</h1>
+  <div className="p-4">
+    <h1 className="text-3xl font-bold text-blue-600">Hello Tailwind</h1>
   </div>
 )
 
